a58cf1d7cd0ed585b00ed2262d536bed8ec45982
Alexi Spanos <ge02040@hotmail.com>
2018-08-19 18:08:48 +0100

    setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='timeseries_toolkit',
       version='0.1.1dev',
       description='Helper library for time series forecasting',
+      long_description = long_description,
+      long_description_content_type="text/markdown",
       install_requires=[open('requirements.txt').read().splitlines()],
       packages=find_packages(),
       author='Alex Spanos',
-      url = 'https://github.com/alejio/timeseries_toolkit',
       author_email='alexi.spanos@yahoo.com',
-      classifiers = [
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
+      url='https://github.com/alejio/timeseries_toolkit',
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
       ]
-)
+      )
